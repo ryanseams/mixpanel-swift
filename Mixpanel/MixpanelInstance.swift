4deb6a8257490bd089d81cbc002e4b8cc903b5e3
Hairuo Sang <hairuo.sang@gmail.com>
2017-07-06 12:58:28 -0700

    Tests now all pass, application also now archives when flush is called

--- Mixpanel/MixpanelInstance.swift
+++ Mixpanel/MixpanelInstance.swift
@@ -853,8 +853,14 @@ extension MixpanelInstance {
         
         NSLog("\(self.eventsQueue.count)  AAAA")
         NSLog("\(self.people.peopleQueue.count)  AAAA")
+        
+        self.archive()
 
         networkQueue.async() {
+            
+            if let shouldFlush = self.delegate?.mixpanelWillFlush(self), !shouldFlush {
+                return
+            }
             
             self.trackingQueue.sync {
                 self.copyEventsQueue = self.eventsQueue
@@ -867,10 +873,6 @@ extension MixpanelInstance {
                 self.people.peopleQueue.removeAll()
             }
             
-            if let shouldFlush = self.delegate?.mixpanelWillFlush(self), !shouldFlush {
-                return
-            }
-            
             #if DECIDE
             self.flushInstance.flushEventsQueue(&self.copyEventsQueue,
                                                 automaticEventsEnabled: self.decideInstance.automaticEventsEnabled)
@@ -879,15 +881,18 @@ extension MixpanelInstance {
                                                 automaticEventsEnabled: false)
             #endif
             self.flushInstance.flushPeopleQueue(&self.copyPeopleQueue)
-            self.archive()
-            
+
             self.trackingQueue.sync {
                 self.eventsQueue = self.copyEventsQueue + self.eventsQueue
                 self.people.peopleQueue = self.copyPeopleQueue + self.copyPeopleQueue
+                self.copyEventsQueue.removeAll()
+                self.copyPeopleQueue.removeAll()
                 NSLog("\(self.eventsQueue.count)  BBBB")
                 NSLog("\(self.people.peopleQueue.count)  BBBB")
-
             }
+            self.archive()
+
+
             
             if let completion = completion {
                 DispatchQueue.main.async(execute: completion)
